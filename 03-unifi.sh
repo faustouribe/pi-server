37713f9938e74875df0b00098f0e00d001662b38
faustouribe <faustouribe@gmail.com>
2022-08-31 20:02:53 -0500

    Create 03-unifi.sh

---
+++ 03-unifi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIRECTORY_BASE="${DIRECTORY_BASE:-$(pwd)}"
+[[ -d "$DIRECTORY_BASE" ]] || mkdir -p "$DIRECTORY_BASE" || { echo "Couldn't create storage directory: $DIRECTORY_BASE"; exit 1; }
+
+docker run -d \
+  --name=unifi-controller \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e MEM_LIMIT=1024 `#optional` \
+  -e MEM_STARTUP=1024 `#optional` \
+  -p 8443:8443 \
+  -p 3478:3478/udp \
+  -p 10001:10001/udp \
+  -p 8080:8080 \
+  -p 1900:1900/udp `#optional` \
+  -p 8843:8843 `#optional` \
+  -p 8880:8880 `#optional` \
+  -p 6789:6789 `#optional` \
+  -p 5514:5514/udp `#optional` \
+  -v "$DIRECTORY_BASE/unifi:/config" \
+  --restart unless-stopped \
+  lscr.io/linuxserver/unifi-controller:latest
