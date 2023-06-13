966f0e45979cac323c0f5eb2775621ec6e806247
faustouribe <faustouribe@gmail.com>
2023-06-13 16:51:31 -0500

    Update 01-pihole.sh

--- 01-pihole.sh
+++ 01-pihole.sh
@@ -18,7 +18,7 @@ docker run -d \
     --hostname pi.hole \
     -e VIRTUAL_HOST="pi.hole" \
     -e PROXY_LOCATION="pi.hole" \
-    -e FTLCONF_LOCAL_IPV4="192.168.1.6" \
+    -e FTLCONF_LOCAL_IPV4="192.168.X.X" \
     pihole/pihole:latest
 
 printf 'Starting up pihole container '
